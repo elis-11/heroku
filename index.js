5f0c2f89a8969a21059cbff21dbf67dc4034d94a
elis-11 <eliza.arzanukaeva@gmail.com>
2021-10-28 11:47:10 +0200

    new changes

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ import express from "express";
 const app = express();
 const port=process.env.PORT ||3011
 
-app.get('/', (req, res) => res.send('Hello our beautiful World!!!'))
+app.get('/', (req, res) => res.send('Hello our beautiful World'))
 
 app.listen(port, () => console.log(`listening on http://localhost:${port}`))
