f8a90d36cbad9d12e5ba1fcfa6d968564621d67f
elis-11 <eliza.arzanukaeva@gmail.com>
2021-10-29 19:41:06 +0200

    modified HEROKU

--- BACK-ANF/index.js
+++ BACK-ANF/index.js
@@ -1,8 +1,8 @@
-import express from "express";
+// import express from "express";
 
-const app = express();
-const port=process.env.PORT ||3011
+// const app = express();
+// const port=process.env.PORT ||3011
 
-app.get('/', (req, res) => res.send('Hello our beautiful World'))
+// app.get('/', (req, res) => res.send('Hello our beautiful World'))
 
-app.listen(port, () => console.log(`listening on http://localhost:${port}`))
+// app.listen(port, () => console.log(`listening on http://localhost:${port}`))
---
+++ README.md
@@ -0,0 +1,15 @@
+# express-node-ejs-json-write
+
+This is an Node/Express site that serves EJS pages including a settings page that allows the user to type in a password which changes a JSON file on the backend via the API.
+
+![grafik](https://user-images.githubusercontent.com/446574/139433615-c3ae3d93-661c-4c94-803f-28e2b4475d24.png)
+
+## Setup
+
+- `npm i`
+- `npm start`
+
+## Publish at Heroku
+
+- `heroku create`
+- `git push heroku master`
