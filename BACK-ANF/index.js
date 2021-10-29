9fb2e62040020e74800eba76accf863ff9f2490f
elis-11 <eliza.arzanukaeva@gmail.com>
2021-10-29 19:17:08 +0200

    created BACK-ANFANG

---
+++ BACK-ANF/index.js
@@ -0,0 +1,8 @@
+import express from "express";
+
+const app = express();
+const port=process.env.PORT ||3011
+
+app.get('/', (req, res) => res.send('Hello our beautiful World'))
+
+app.listen(port, () => console.log(`listening on http://localhost:${port}`))
