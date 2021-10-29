2458feb9e0ee44c258ecc0b6e687a89e2d197092
elis-11 <eliza.arzanukaeva@gmail.com>
2021-10-29 19:16:35 +0200

    deleted some files

--- index.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-
-const app = express();
-const port=process.env.PORT ||3011
-
-app.get('/', (req, res) => res.send('Hello our beautiful World'))
-
-app.listen(port, () => console.log(`listening on http://localhost:${port}`))
