2dc04c2cf5e0d3a3f379122d9f0f02823eb10db7
elis-11 <eliza.arzanukaeva@gmail.com>
2021-11-09 16:24:14 +0100

    new changes

--- heroku/src/qtools/qfil.js
+++ heroku/src/qtools/qfil.js
@@ -1,5 +1,5 @@
 import fs from 'fs';
- 
+
 export const getJsonDataFromFile = (fileName, callback) => {
     fs.readFile(`./src/data/${fileName}`, 'utf-8', (err, rawData) => {
         const data = JSON.parse(rawData);
